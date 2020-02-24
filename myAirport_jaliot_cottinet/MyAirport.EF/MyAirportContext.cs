7b603877c053e80f471588b35493bb290b4b4763
Roman Cottinet <romancottinet@gmail.com>
2020-02-24 10:18:22 +0100

    ajout classe Bagage et vol + dbcontext

---
+++ myAirport_jaliot_cottinet/MyAirport.EF/MyAirportContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace JC.MyAirport.EF
+{
+    class MyAirportContext : DbContext
+    {
+            public DbSet<Vol> Vols { get; set; }
+            public DbSet<Bagage> Bagages { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=MyAirport;Integrated Security=True");
+            }
+    }
+}
---
+++ myAirport_jaliot_cottinet/MyAirport.EF/Vol.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JC.MyAirport.EF
+{
+    class Vol
+    {
+    }
+}
