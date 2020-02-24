ac593df485700973854f4b77be4a6447243eace2
Bryan93420 <jaliot.bryan@gmail.com>
2020-02-24 10:52:20 +0100

    Ajout attribut Bagage

--- myAirport_jaliot_cottinet/MyAirport.EF/Bagage.cs
+++ myAirport_jaliot_cottinet/MyAirport.EF/Bagage.cs
@@ -6,5 +6,18 @@ namespace JC.MyAirport.EF
 {
     class Bagage
     {
+        public int id_bagage { get; set; }
+        
+        public Vol vol { get; set; }
+        public int id_vol { get; set; }
+        public string code_iata { get; set; }
+        public DateTime date_creation { get; set; }
+        public string classe { get; set; }
+        public Boolean prioritaire { get; set; }
+        public string sta { get; set; }
+        public string ssur { get; set; }
+        public string destination { get; set; }
+        public string escale { get; set; }
+
     }
 }
