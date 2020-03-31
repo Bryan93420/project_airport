ef563b78d67497c6a5cd8f8618fa033cd8562bc4
Roman Cottinet <romancottinet@gmail.com>
2020-03-31 10:25:49 +0200

    creation de l'api rest EF et ajout controlleurs Vol et Bagage

--- myAirport_jaliot_cottinet/MyAirport.EF/MyAirportContext.cs
+++ myAirport_jaliot_cottinet/MyAirport.EF/MyAirportContext.cs
@@ -10,22 +10,20 @@ namespace JC.MyAirport.EF
 {
     public class MyAirportContext : DbContext
     {
-        /*public MyAirportContext(DbContextOptions<MyAirportContext> options) : base(options) 
-        { 
+        public MyAirportContext(DbContextOptions<MyAirportContext> options) : base(options) 
+        { }
 
-        }*/
-
-        
         public DbSet<Vol> Vols { get; set; }
         public DbSet<Bagage> Bagages { get; set; }
 
         public static readonly ILoggerFactory MyAirportLoggerFactory
         = LoggerFactory.Create(builder => { builder.AddConsole(); });
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["MyAirportContext"].ConnectionString);
             optionsBuilder.UseLoggerFactory(MyAirportLoggerFactory);
 
-        }
+        }*/
     }
 }
